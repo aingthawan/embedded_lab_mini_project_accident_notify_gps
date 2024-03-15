6f02bf8eee78c6e68464ceeb8f46b00d145b8470
aingez <s6401012610042@email.kmutnb.ac.th>
2024-03-15 22:45:16 +0700

    apply kalman's filter
    ref : https://github.com/CarbonAeronautics/Part-XV-1DKalmanFilter/blob/main/ArduinoCode

--- gy521_test/gy521_test.ino
+++ gy521_test/gy521_test.ino
@@ -9,6 +9,21 @@ float PrevAccX, PrevAccY, PrevAccZ; // Variables to store previous accelerometer
 
 float LoopTimer;
 
+float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
+int RateCalibrationNumber;
+float KalmanAngleRoll=0, KalmanUncertaintyAngleRoll=2*2;
+float KalmanAnglePitch=0, KalmanUncertaintyAnglePitch=2*2;
+float Kalman1DOutput[]={0,0};
+void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
+  KalmanState=KalmanState+0.004*KalmanInput;
+  KalmanUncertainty=KalmanUncertainty + 0.004 * 0.004 * 4 * 4;
+  float KalmanGain=KalmanUncertainty * 1/(1*KalmanUncertainty + 3 * 3);
+  KalmanState=KalmanState+KalmanGain * (KalmanMeasurement-KalmanState);
+  KalmanUncertainty=(1-KalmanGain) * KalmanUncertainty;
+  Kalman1DOutput[0]=KalmanState; 
+  Kalman1DOutput[1]=KalmanUncertainty;
+}
+
 void gyro_update(void) {
   // EN lowpass filter
   Wire.beginTransmission(0x68);
@@ -50,12 +65,10 @@ void gyro_update(void) {
   // Angle Measurement
   AngleRoll=atan( AccY / sqrt(AccX*AccX + AccZ*AccZ))*1 / (3.142/180);
   AnglePitch=atan( AccX / sqrt(AccY*AccY + AccZ*AccZ))*1 / (3.142/180);
-  // Around x
-  Serial.print("Roll [°] = ");
-  Serial.print(AngleRoll);
-  // Around y
-  Serial.print(" Pitch [°] = ");
-  Serial.println(AnglePitch);
+  // Serial.print("Roll [째] = "); // Around X
+  // Serial.print(AngleRoll);
+  // Serial.print(" Pitch [째] = "); // Around Y
+  // Serial.println(AnglePitch);
   
   // Store current accelerometer readings for next iteration
   PrevAccX = AccX;
@@ -72,29 +85,52 @@ void setup() {
   Wire.write(0x6B);
   Wire.write(0x00);
   Wire.endTransmission();
+
+  for (RateCalibrationNumber=0; RateCalibrationNumber<2000; RateCalibrationNumber ++) {
+    gyro_update();
+    RateCalibrationRoll+=RateRoll;
+    RateCalibrationPitch+=RatePitch;
+    RateCalibrationYaw+=RateYaw;
+    delay(1);
+  }
+  RateCalibrationRoll/=2000;
+  RateCalibrationPitch/=2000;
+  RateCalibrationYaw/=2000;
+  LoopTimer=micros();
 }
 
 void loop() {
   gyro_update();
   
-  // Check if orientation is unsafe
-  if (abs(AngleRoll) > 45 || abs(AnglePitch) > 45 || AccZ < -0.7) {
-    Serial.println("Orientation Unsafe!"); 
-    // tone(15, 1000, 5);
-  }
+  // // Check if orientation is unsafe
+  // if (abs(AngleRoll) > 45 || abs(AnglePitch) > 45 || AccZ < -0.7) {
+  //   Serial.println("Orientation Unsafe!"); 
+  // }
 
-  // Detect collision
-  float accChangeX = abs(AccX - PrevAccX);
-  float accChangeY = abs(AccY - PrevAccY);
-  float accChangeZ = abs(AccZ - PrevAccZ);
-  
-  // Set a threshold for collision detection
-  float collisionThreshold = 2.0; // Adjust this value as needed
-  
-  if (accChangeX > collisionThreshold || accChangeY > collisionThreshold || accChangeZ > collisionThreshold) {
-    Serial.println("Collision Detected!");
-    // Perform actions when collision is detected
-  }
+  // // Detect collision
+  // float accChangeX = abs(AccX - PrevAccX);
+  // float accChangeY = abs(AccY - PrevAccY);
+  // float accChangeZ = abs(AccZ - PrevAccZ);
+  // float collisionThreshold = 2.0; // Adjust threshold
+  // if (accChangeX > collisionThreshold || accChangeY > collisionThreshold || accChangeZ > collisionThreshold) {
+  //   Serial.println("Collision Detected!");
+  // }
 
-  delay(50);
+  // delay(50);
+
+  RateRoll-=RateCalibrationRoll;
+  RatePitch-=RateCalibrationPitch;
+  RateYaw-=RateCalibrationYaw;
+  kalman_1d(KalmanAngleRoll, KalmanUncertaintyAngleRoll, RateRoll, AngleRoll);
+  KalmanAngleRoll=Kalman1DOutput[0]; 
+  KalmanUncertaintyAngleRoll=Kalman1DOutput[1];
+  kalman_1d(KalmanAnglePitch, KalmanUncertaintyAnglePitch, RatePitch, AnglePitch);
+  KalmanAnglePitch=Kalman1DOutput[0]; 
+  KalmanUncertaintyAnglePitch=Kalman1DOutput[1];
+  Serial.print("Roll Angle : ");
+  Serial.print(KalmanAngleRoll);
+  Serial.print(" Pitch Angle : ");
+  Serial.println(KalmanAnglePitch);
+  while (micros() - LoopTimer < 4000);
+  LoopTimer=micros();
 }
